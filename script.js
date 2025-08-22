8ce8e7fe528f726d8e405c185dbc698d2bcd98e4
Mxmthe <xmirzaraxmonov@gmail.com>
2025-08-22 23:35:32 +0500

    feat:add some features

--- script.js
+++ script.js
@@ -1,20 +1,74 @@
 const form = document.getElementById('creator');
-const charName = document.getElementById('charName');
+if (form) {
+  const charName = document.getElementById('charName');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const name = charName.value.trim();
-  // простая валидация
-  if (!/^[a-zA-Z0-9 _-]{2,16}$/.test(name)) {
-    alert('Имя должно быть 2–16 символов: буквы/цифры/пробел/-/_');
-    return;
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = charName.value.trim();
+
+    if (!/^[a-zA-Z0-9 _-]{2,16}$/.test(name)) {
+      alert('Имя должно быть 2–16 символов: буквы/цифры/пробел/-/_');
+      return;
+    }
+    localStorage.setItem('char_name', name);
+    window.location.href = 'main-menu.html';
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const view = document.getElementById('view');
+  if (!view) return; // если это не main-menu, выходим
+
+  const actions = document.querySelector('.right-menu__content');
+  const name = localStorage.getItem('char_name') || 'Player';
+
+  render('home');
+
+  actions.addEventListener('click', (e) => {
+    const btn = e.target.closest('[data-view]');
+    if (!btn) return;
+    render(btn.dataset.view);
+  });
+
+  function render(which) {
+    switch (which) {
+      case 'profile': renderProfile(); break;
+      case 'settings': renderSettings(); break;
+      case 'home':
+      default: renderHome(); break;
+    }
   }
-  // 1) сохраняем (если хочешь передать в другую страницу/игру)
-  localStorage.setItem('char_name', name);
 
-  // 2) переход на страницу игры (замени на путь к твоей игре)
-  window.location.href = 'main-menu.html';
+  function renderHome() {
+    console.log("Render Home!");
+    view.innerHTML = `
+      <section class="home-screen">
+        <button class="fight-btn">Fight!</button>
+      </section>
+    `;
+  }
 
-  // или просто показать, что всё сработало:
-  // alert('Создан персонаж: ' + name);
+  function renderProfile() {
+    view.innerHTML = `
+      <section class="profile-screen">
+        <div class="avatar-wrap">
+          <img src="images/hero1.jpg" alt="Avatar" class="avatar">
+        </div>
+        <div class="profile-info">
+          <p>Name: <strong>${name}</strong></p>&nbsp;&nbsp;
+          <p>
+            Wins: <span class="win">1</span> &nbsp;&nbsp;
+            Losses: <span class="lose">1</span>
+          </p>
+        </div>
+
+        
+      </section>
+    `;
+  }
+
+  function renderSettings() {
+    view.innerHTML = `<h2>Settings</h2><p>Coming soon…</p>`;
+  }
 });
+
